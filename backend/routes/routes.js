0804595ec95eef706fd7fc5118c1b7e7ac623bbb
Arnaiz23 <aaf110501@gmail.com>
2022-09-05 00:57:59 +0200

    Update

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,8 +8,6 @@ router.get("/", async (req, res) => {
   res.status(200).send("Hello World")
 })
 
-// ! Catch
-
 // ? Get contacts of a user
 router.get("/contacts/:id", async (req, res) => {
   const { id } = req.params
